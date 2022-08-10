46d44915ea90041af8711f762c96dfb6e5634e57
ragav <ragavaraaj@gmail.com>
2022-08-10 10:59:01 +0530

    getting a specific artist

--- crud/artist.py
+++ crud/artist.py
@@ -1,6 +1,6 @@
 from uuid import uuid1
 from sqlalchemy.orm import Session
-from models import Artist
+from models import Artist, Role
 from schemas.artist import PostArtist
 
 
@@ -8,12 +8,12 @@ def get_artist(db: Session, artist_id: int):
     return db.query(Artist).filter(Artist.id == artist_id).first()
 
 
-# def get_artist_id(db: Session, artist_desc: str):
-#     return db.query(Artist).filter(Artist.desc == artist_desc).first()
-
-
-def get_artists(db: Session):
-    return db.query(Artist).all()
+def get_specific_artists(db: Session, type: str):
+    if(type == None):
+        return db.query(Artist).all()
+    else:
+        print(db.query(Artist).join(Role).filter(Role.desc == "Actor"))
+        return db.query(Artist).join(Role).filter(Role.desc == "Actor").all()
 
 
 def create_artist(db: Session, data: PostArtist):
